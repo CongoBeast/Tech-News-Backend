43b1bde02007473a03466e2a89cd6b11bf1d3546
Methembe Tshuma <53082789+CongoBeast@users.noreply.github.com>
2024-08-14 08:54:20 +0200

    Update server.js

--- server.js
+++ server.js
@@ -244,20 +244,20 @@ app.get('/get-articles', (req, res) => {
     "collection": "TechNews",
     "database": "thomastshuma43",
     "dataSource": "Cluster0",
-    // "filter": {
-    //     "$gte": { "$date": startOfMonth },
-    //     "$lt": { "$date": endOfMonth }
-    // }
-    "pipeline": [
-      {
-        "$match": {
-          "date": {
-            "$gte": { "$date": startOfMonth },
-            "$lt": { "$date": endOfMonth }
-          }
-        }
-      }
-    ]
+    "filter": {
+        // "$gte": { "$date": startOfMonth },
+        // "$lt": { "$date": endOfMonth }
+    }
+    // "pipeline": [
+    //   {
+    //     "$match": {
+    //       "date": {
+    //         "$gte": { "$date": startOfMonth },
+    //         "$lt": { "$date": endOfMonth }
+    //       }
+    //     }
+    //   }
+    // ]
   });
 
   // axios({ ...apiConfig, url: `${apiConfig.urlBase}find`, data })
